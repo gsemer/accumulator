cd6e476db4de9090e3777d1c15cfb16cc135c0d1
gsemer <gsemertzakis@hotmail.com>
2025-12-10 18:48:03 +0200

    test get

--- domain/states_test.go
+++ domain/states_test.go
@@ -32,3 +32,33 @@ func TestAdd(t *testing.T) {
 		t.Errorf("expected %v, got %v", int64(n*(n+1)/2), state.accumulator)
 	}
 }
+
+func TestGet(t *testing.T) {
+	state := NewState()
+
+	state.Add(10)
+	state.Add(20)
+
+	addStarted := make(chan struct{})
+	addDone := make(chan struct{})
+
+	go func() {
+		addStarted <- struct{}{}
+		state.Add(100) // takes 1 second before locking
+		addDone <- struct{}{}
+	}()
+
+	<-addStarted
+
+	sum, _ := state.Get("sum")
+	if sum.(int64) != 30 {
+		t.Errorf("expected %v, got %v instead", 30, sum)
+	}
+
+	list, _ := state.Get("list")
+	if len(list.([]int64)) != 2 || list.([]int64)[0] != 10 || list.([]int64)[1] != 20 {
+		t.Errorf("expected [10, 20], got %v instead", list)
+	}
+
+	<-addDone
+}
